f14e6aa7e0a2b92a2a5f9a4bf43a1f6141daa871
kylemocode <kylemo860617@gmail.com>
2021-02-08 23:56:25 +0800

    chore: clean code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is Kyle Mo's deployment sample!
+          This is Kyle Mo's deployment sample!!
         </p>
         <a
           className="App-link"
